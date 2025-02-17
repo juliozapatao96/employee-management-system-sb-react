2a392b36d87612efe9cf308ce792d93d3064a412
JulioZapata <julio.cesar.zaor@gmail.com>
2025-02-16 21:00:16 -0500

    Init: An HTTP POST endpoint is defined to create employees

--- src/main/java/net/javajz/ems/service/impl/EmployeeServiceImpl.java
+++ src/main/java/net/javajz/ems/service/impl/EmployeeServiceImpl.java
@@ -1,5 +1,24 @@
 package net.javajz.ems.service.impl;
 
-public class EmployeeServiceImpl {
+import lombok.AllArgsConstructor;
+import net.javajz.ems.dto.EmployeeDto;
+import net.javajz.ems.entity.Employee;
+import net.javajz.ems.mapper.EmployeeMapper;
+import net.javajz.ems.repository.EmployeeRepository;
+import net.javajz.ems.service.EmployeeService;
+import org.springframework.stereotype.Service;
 
+@Service
+@AllArgsConstructor
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public EmployeeDto createEmployee(EmployeeDto employeeDto) {
+
+        Employee employee = EmployeeMapper.mapToEmployee(employeeDto);
+        Employee savedEmployee = employeeRepository.save(employee);
+        return EmployeeMapper.mapToEmployeeDto(savedEmployee);
+    }
 }
