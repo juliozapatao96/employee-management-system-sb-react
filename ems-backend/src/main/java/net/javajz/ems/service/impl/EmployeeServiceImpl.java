21628ed80612c1a2e8f417dc28676025e37a3954
JulioZapata <julio.cesar.zaor@gmail.com>
2025-02-20 22:44:18 -0500

    feate: create and implement a service to delete an employee

--- ems-backend/src/main/java/net/javajz/ems/service/impl/EmployeeServiceImpl.java
+++ ems-backend/src/main/java/net/javajz/ems/service/impl/EmployeeServiceImpl.java
@@ -54,4 +54,13 @@ public class EmployeeServiceImpl implements EmployeeService {
 
         return EmployeeMapper.mapToEmployeeDto(updateEmployeeObj);
     }
+
+    @Override
+    public void deleteEmployee(Long employeeId) {
+        Employee employee = employeeRepository.findById(employeeId).orElseThrow(
+                () -> new ResourceNotFoundException("Employee is not exists with given id : " + employeeId)
+        );
+
+        employeeRepository.deleteById(employeeId);
+    }
 }
