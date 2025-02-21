a1f1f45d09f2cc2f2df11f9c230a8d1639b05b82
JulioZapata <julio.cesar.zaor@gmail.com>
2025-02-20 20:16:12 -0500

    feat: create method to get all employees

--- ems-backend/src/main/java/net/javajz/ems/service/EmployeeService.java
+++ ems-backend/src/main/java/net/javajz/ems/service/EmployeeService.java
@@ -2,8 +2,12 @@ package net.javajz.ems.service;
 
 import net.javajz.ems.dto.EmployeeDto;
 
+import java.util.List;
+
 public interface EmployeeService {
     EmployeeDto createEmployee(EmployeeDto employeeDto);
 
     EmployeeDto getEmployeeById(Long employeeId);
+
+    List<EmployeeDto> getAllEmployees();
 }
