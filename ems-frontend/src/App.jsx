47dd26d71f1f6de3776631ccf8ea6e0210d3ebd3
juliozapatao96 <julio.cesar.zaor@gmail.com>
2025-02-23 15:36:20 -0500

    feat: add employee component and routing

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import FooterComponent from './components/FooterComponent'
 import HeaderComponent from './components/HeaderComponent'
+import AddEmployeeComponent from './components/AddEmployeeComponent'
 import ListEmployeeComponent from './components/ListEmployeeComponent'
 
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
@@ -16,6 +17,8 @@ function App() {
           <Route path='/' element={<ListEmployeeComponent/>}></Route>
           {/* // http://localhost:3000/employees */}
           <Route path='/employees' element={<ListEmployeeComponent/>}></Route>
+          {/* // http://localhost:3000/add-employee */}
+          <Route path='/add-employee' element={<AddEmployeeComponent/>}></Route>
         </Routes>
         <FooterComponent/>
      </BrowserRouter>
